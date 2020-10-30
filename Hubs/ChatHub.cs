0f2e479cdb7a9afbbf8a13ef156c69c9acccba1f
Faner Yedermann <faner.yedermann@gmail.com>
2020-10-30 22:50:27 +0100

    Initial commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            Console.WriteLine($"Received a message from {user}: {message}");
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine("Connection established.");
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Console.WriteLine("Connection closed.");
+            return base.OnDisconnectedAsync(exception); 
+        }
+    }
+}
